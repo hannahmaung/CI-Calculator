d6614ce369a4cc1f5e1ecc376a121263629aede1
hannahmaung <hannahmaung33@gmail.com>
2021-06-06 15:41:43 -0700

    adding test

---
+++ test_calculator.py
@@ -0,0 +1,16 @@
+"""
+Tests for calculator app
+"""
+
+import calculator 
+
+
+class TestCalculatorApp:
+    def test_add(self):
+        assert 5 == calculator.add(3,2)
+    def test_subtract(self):
+        assert 5 == calculator.subtract(10,5)
+    def test_mult(self):
+        assert 8 == calculator.multiply(2,4)
+    def test_divide(self):
+        assert 10 == calculator.divide(20,2)
